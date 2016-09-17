b561d61bc9052d8800980b08d57bdc05d278995e
icoxfog417 <icoxfog417@yahoo.co.jp>
2016-09-17 17:40:42 +0900

    add gpu option

--- run.py
+++ run.py
@@ -10,17 +10,17 @@ from model.dqn_trainer import DQNTrainer
 PATH = os.path.join(os.path.dirname(__file__), "./store")
 
 
-def run(render):
+def run(render, gpu):
     env = Environment()
-    agent = DQNAgent(env.actions, epsilon=0.1, model_path=PATH)
+    agent = DQNAgent(env.actions, epsilon=0.1, model_path=PATH, on_gpu=gpu)
 
     for ep, s, r in env.play(agent, render=render):
         pass
 
 
-def train(render):
+def train(render, gpu):
     env = Environment()
-    agent = DQNAgent(env.actions, epsilon=1, model_path=PATH)
+    agent = DQNAgent(env.actions, epsilon=1, model_path=PATH, on_gpu=gpu)
     trainer = DQNTrainer(agent)
 
     for ep, s, r in env.play(trainer, episode=10**5, render=render, report_interval=10):
@@ -31,10 +31,11 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Pong DQN")
     parser.add_argument("--render", action="store_const", const=True, default=True, help="render or not")
     parser.add_argument("--train", action="store_const", const=True, default=False, help="train or not")
+    parser.add_argument("--gpu", action="store_const", const=True, default=False, help="user gpu or not")
     args = parser.parse_args()
 
     if args.train:
-        train(args.render)
+        train(args.render, args.gpu)
     else:
-        run(args.render)
+        run(args.render, args.gpu)
 
