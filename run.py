77375a1ad3648d981200a25dc77da01304057d34
icoxfog417 <icoxfog417@yahoo.co.jp>
2016-09-17 17:54:04 +0900

    disable default render

--- run.py
+++ run.py
@@ -29,7 +29,7 @@ def train(render, gpu):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Pong DQN")
-    parser.add_argument("--render", action="store_const", const=True, default=True, help="render or not")
+    parser.add_argument("--render", action="store_const", const=True, default=False, help="render or not")
     parser.add_argument("--train", action="store_const", const=True, default=False, help="train or not")
     parser.add_argument("--gpu", action="store_const", const=True, default=False, help="user gpu or not")
     args = parser.parse_args()
