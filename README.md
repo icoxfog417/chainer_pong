36c7f95cc69b3a4f54960c12c820ef25ea7a473e
icoxfog417 <icoxfog417@yahoo.co.jp>
2016-09-21 10:57:02 +0900

    implements submit process

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# chainer_pong
-learn pong by chainer
+# chainer pong
+
+DQN implementation by Chainer.
+
+![model.PNG](./model.PNG)
+
+The result is uploaded to OpenAI Gym.
+
+[Link to Page](https://gym.openai.com/evaluations/eval_V4ZjiW90Tyh1WRZQEGSQ)
--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import argparse
+import gym
 sys.path.append(os.path.join(os.path.dirname(__file__), "../"))
 from model.environment import Environment
 from model.dqn_agent import DQNAgent
@@ -10,12 +11,21 @@ from model.dqn_trainer import DQNTrainer
 PATH = os.path.join(os.path.dirname(__file__), "./store")
 
 
-def run(render, gpu):
+def run(submit_key, gpu):
     env = Environment()
     agent = DQNAgent(env.actions, epsilon=0.05, model_path=PATH, on_gpu=gpu)
+    path = ""
+    episode = 5
+    if submit_key:
+        print("make directory to submit result")
+        path = os.path.join(os.path.dirname(__file__), "submit")
+        episode = 100
 
-    for ep, s, r in env.play(agent, episode=5, render=True):
+    for ep, s, r in env.play(agent, episode=episode, render=True, action_interval=4, record_path=path):
         pass
+    
+    if submit_key:
+        gym.upload(path, api_key=submit_key)
 
 
 def train(render, gpu):
@@ -30,6 +40,7 @@ def train(render, gpu):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Pong DQN")
     parser.add_argument("--render", action="store_const", const=True, default=False, help="render or not")
+    parser.add_argument("--submit", type=str, default="", help="api key to submit data")
     parser.add_argument("--train", action="store_const", const=True, default=False, help="train or not")
     parser.add_argument("--gpu", action="store_const", const=True, default=False, help="user gpu or not")
     args = parser.parse_args()
@@ -37,5 +48,5 @@ if __name__ == "__main__":
     if args.train:
         train(args.render, args.gpu)
     else:
-        run(args.render, args.gpu)
+        run(args.submit, args.gpu)
 
